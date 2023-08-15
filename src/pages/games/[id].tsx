4b962e309784aadcb5f4b92907d0730c24992faa
So8oS <Ahmadothman033@gmail.com>
2023-08-16 01:29:32 +0400

    fix

--- src/pages/games/[id].tsx
+++ src/pages/games/[id].tsx
@@ -4,11 +4,13 @@ import { videos } from "./../../games";
 
 const Game = () => {
   const router = useRouter();
+  // @ts-ignore
   const id = parseInt(router.query.id);
   const game = videos.find((game) => game.id === id);
   console.log(id);
   return (
     <div className=" flex flex-col justify-center items-center">
+      {/* @ts-ignore */}
       <iframe className="w-full h-screen" src={game?.links[0].url} frameBorder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe>
     </div>
   );
