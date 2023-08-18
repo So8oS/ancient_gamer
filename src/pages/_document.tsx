8746358198e6e54d340b36d4e626540d57ada9dc
So8oS <Ahmadothman033@gmail.com>
2023-08-18 14:34:56 +0400

    icon added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet" />
+      <link rel="shortcut icon" href="/icon.ico" />
       {/* @ts-ignore */}
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9453766584853046" crossorigin="anonymous"></script>
       <body>
