fd7c189cc7b23d62bba542a341de7923b6e78314
Ahmad Othman <62267611+So8oS@users.noreply.github.com>
2023-08-24 02:39:59 +0400

    Update [id].tsx

--- src/pages/games/[id].tsx
+++ src/pages/games/[id].tsx
@@ -24,7 +24,11 @@ const Game = () => {
         mozallowfullscreen="true"
         scrolling="no"
       ></iframe>
-      <h1 className="bg-orange-800 p-4 rounded-2xl mt-5 ">Right click/long press {"=>"} Enter Full Screen to play with full screen </h1>
+
+      <h1 className="bg-black p-2 rounded-2xl mt-5 ">All of the games are embedded from different websites  </h1>
+
+
+      <h1 className="bg-orange-800 p-4 rounded-2xl mt-2 ">Right click/long press {"=>"} Enter Full Screen to play with full screen </h1>
     </div>
   );
 };
