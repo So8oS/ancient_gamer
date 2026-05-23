d7cf963f196fc64ef1e8d0f3fd01b9e2c29c4ff9
so8os <ahmadothman033@gmail.com>
2026-05-23 23:07:14 +0400

    sw file added

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11044307
+    "zoneId": 11048322
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
